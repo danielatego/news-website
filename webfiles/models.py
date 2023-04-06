a8ba9a82186488a54f7a8aeb8f0724c181e3827f
danielatego <danielatego75@gmail.com>
2023-04-06 08:01:10 +0300

    update

--- webfiles/models.py
+++ webfiles/models.py
@@ -16,7 +16,7 @@ class Subscriber(db.Model):
     password_hash = db.Column(db.String, nullable= False)
     comments = db.relationship('Comment', backref='commenter', lazy=True)
     liker = db.relationship('Likess', backref='likerr', lazy=True)
-    last_visit=db.Column(db.DateTime )
+    last_visit=db.Column(db.DateTime,default=datetime.now )
     viewerreg= db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __init__(self, user_name, user_email,
